af364b8e4304dfee01eada624fe198ab6f77fd43
Amanpreet Singh <51552560+Singhamanpreet2002@users.noreply.github.com>
2023-08-11 17:49:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Letterize from "https://cdn.skypack.dev/letterizejs@2.0.0";
+import Letterize from "https://cdn.skypack.dev/letterizej4s@2.0.0";
 const test = new Letterize({
         targets: ".animate-me"
       });
@@ -7,9 +7,9 @@ const test = new Letterize({
         targets: test.listAll,
         delay: anime.stagger(150, {
           grid: [test.list[0].length, test.list.length],
-          from: "center"
+          from:"center"
         }),
-        loop: 5
+        loop: 10
       });
 
       animation
@@ -35,12 +35,63 @@ const test = new Letterize({
         var textWrapper = document.querySelector('.ml9 .letters');
         textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-        anime.timeline({loop: 3})
+        anime.timeline({loop: false})
           .add({
             targets: '.ml9 .letter',
             scale: [0, 1],
-            duration: 1500,
-            elasticity: 600,
-            delay: (el, i) => 45 * (i+1)
+            duration: 1200,
+            elasticity: 1000,
+            delay: 4000
           })
-          
+          ScrollReveal({reset: false}).reveal('.headline',{ delay: 3000 });
+          ScrollReveal().reveal('.tagline', { delay: 3500 });
+          ScrollReveal().reveal('.para', { interval: 12, reset: true });
+          ScrollReveal().reveal('.container-1', { easing: 'ease-in-out',delay: 1000,reset: true });
+
+
+var cursor = $(".cursor"),
+follower = $(".cursor-follower");
+
+var posX = 0,
+  posY = 0,
+  mouseX = 0,
+  mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+      posX += (mouseX - posX) / 9;
+      posY += (mouseY - posY) / 9;
+
+      TweenMax.set(follower, {
+          css: {
+              left: posX - 20,
+              top: posY - 20
+          }
+      });
+
+      TweenMax.set(cursor, {
+          css: {
+              left: mouseX,
+              top: mouseY
+          }
+      });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
+
+$(".portfolio-item img").on("mouseenter", function() {
+  cursor.addClass("active");
+  follower.addClass("active");
+});
+
+$(".portfolio-item img").on("mouseleave", function() {
+  cursor.removeClass("active");
+  follower.removeClass("active");
+});
+
+  
